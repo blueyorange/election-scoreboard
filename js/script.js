b7fe611041dff0548d2838ad46f6a91b4481f1db
blueyorange <blueyorange@gmail.com>
2021-02-21 07:55:03 +0000

    loads next file on click

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
-  // define number of main parties to display
-  // results separately
+  // define number of main parties n to display
+  // results separately (n+1 bars)
   numberOfMainParties = 3;
   totalSeats = 650;
+  seatsToWin = 326;
+  parties = [];
   // define colours for main parties
   colours = {
     CON : "#0575c9",
     LAB : "#e91d0e",
     LD : "#f8ed2e",
-    OTH : "#aaaaaa"
   }
   // Party class definition
   class Party {
-    constructor(partyCode,partyColour) {
+    constructor(partyCode) {
       this.partyCode = partyCode;
       this.votes = 0;
       this.total = 0;
       this.seats = 0;
       this.share = 0;
-      this.position = 1;
-      // default color is grey
-      this.colour = partyColour;
+      partyCode = $(this).text().trim();
+      if (partyCode in colours) {
+        this.colour = colours[partyCode];
+      } else {
+        // default colour if party colour undefined
+        this.colour = "#aaaaaa";
+      }
     }
 
     add_votes(votes) {
@@ -39,9 +44,14 @@ $(document).ready(function() {
   // Event listener for file selector
   $('#uploader').change(function(event) {
     var fileList = event.target.files;
-    loadAsText(fileList[0]);
-    $('document').click(function() {
-      console.log("Clicked: loading next file.")
+    fileIndex = 0;
+    loadAsText(fileList[fileIndex]);
+
+    // event listener to load files sequentially
+    $(document).on("click",function() {
+      console.log("Clicked: loading next file.");
+      fileIndex += 1;
+      loadAsText(fileList[fileIndex]);
     })
   })
 
@@ -59,8 +69,10 @@ $(document).ready(function() {
     reader.onload = function(loadedEvent) {
       // New file has been loaded
       // Call function to update data and page
-      var constituencyResultXML = loadedEvent.target.result
-      parties = createParties(constituencyResultXML);
+      var constituencyResultXML = loadedEvent.target.result;
+      if (fileIndex == 0) {
+        parties = createParties(constituencyResultXML);
+      }
       update(constituencyResultXML, parties);
     }
     reader.readAsText(file);
@@ -72,13 +84,9 @@ $(document).ready(function() {
     // and colours
     var parties = {};
     $(firstResult).find("partyCode").each(function() {
-      partyCode = $(this).text().trim();
-      if (partyCode in colours) {
-        var colour = colours[partyCode];
-      } else {
-        var colour = colours['OTH'];
-      }
-      parties[partyCode] = new Party(partyCode,colour);
+      var partyCode = $(this).text().trim();
+      console.log("Creating party: ",partyCode);
+      parties[partyCode] = new Party( partyCode );
     })
     return parties;
   }
@@ -121,7 +129,6 @@ $(document).ready(function() {
     })
     // update vote share for each party
     for (party in parties) {
-      console.log(party);
       parties[party].update_vote_share(total_votes);
     }
     console.log(parties);
@@ -134,7 +141,7 @@ $(document).ready(function() {
     console.log(results_by_voteshare);
 
     // Create histogram and results table
-    var $histogram = $('<div></div>')
+    var $histogram = $('<div class="histogram"></div>')
     var $tableHeadRow = $('<thead><tr></tr></thead>');
     var $tableBodyRow = $('<tbody><tr></tr></tbody');
     var barContainerWidth = 100/(numberOfMainParties+1) + '%';
@@ -142,17 +149,17 @@ $(document).ready(function() {
     console.log(height);
     results_by_seat.forEach(function(party) {
       $barContainer = $('<div class="bar-container"></div').width(barContainerWidth);
-      var barHeight = party.seats / totalSeats * height;
-      $bar = $('<div class="bar"></div>').css('background-color',party.colour).height(barHeight);
-      $histogram.append($barContainer).append($bar);
+      var barHeight = parseInt(party.seats / totalSeats * height);
+      $bar = $('<div class="bar"></div>').css('background-color',party.colour).height(barHeight*10);
+      $barContainer.append($bar);
+      $histogram.append($barContainer);
       $tableHeadRow.append($('<th scope="col"></th>').text(party.partyCode));
       $tableBodyRow.append($('<td></td>').text(party.seats));
     })
     // append seats data to table
-    $('.histogram').append($histogram);
-    $('.seats-table').append($tableHeadRow);
+    $('.histogram').html($histogram);
+    $('.seats-table').html($tableHeadRow);
     $('.seats-table').append($tableBodyRow);
   }  
 })
-
 
